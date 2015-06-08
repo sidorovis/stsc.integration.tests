44b37381c0b64111928545fbeac2c4b4eb779649
Ivan Sidarau <ivan.sidarau@gmail.com>
2015-06-08 22:46:05 +0200

    moved all algorithms to <...>/eod/AlgorithmName, <...>/stock/AlgorithmName

---
+++ src/test/java/stsc/integration/tests/algorithms/privitive/eod/SimpleTradingAlgorithmTest.java
@@ -0,0 +1,23 @@
+package stsc.integration.tests.algorithms.privitive.eod;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import stsc.algorithms.primitive.eod.TestingEodAlgorithm;
+import stsc.common.BadSignalException;
+import stsc.common.Day;
+import stsc.common.algorithms.BadAlgorithmException;
+import stsc.integration.tests.helper.EodAlgoInitHelper;
+
+public class SimpleTradingAlgorithmTest {
+
+	@Test
+	public void testTestingEodAlgorithm() throws BadSignalException, BadAlgorithmException {
+		final EodAlgoInitHelper init = new EodAlgoInitHelper("eName");
+
+		final TestingEodAlgorithm tea = new TestingEodAlgorithm(init.getInit());
+		tea.process(new Date(), new HashMap<String, Day>());
+	}
+}
